6ce2f653545d4c81da66ed06bedc56ec4ba67c8a
Jerry G <jerry99george@gmail.com>
2024-09-24 11:51:54 +0100

    swapped the angles around, just so it looks better

---
+++ project_info.js
@@ -0,0 +1,14 @@
+projectInformation = [
+    {
+        name:"Maze Escape Raycaster",
+        description:"Small maze game that works with raycasting.",
+        file:"maze.html",
+        image:"maze_demo.gif"
+    },
+    {
+        name:"Cow Rotator",
+        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
+        file:"cow-rotator.html",
+        image:"cow.gif"
+    }
+]
