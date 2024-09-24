fa5d17972c2fb34b176ed791ed1864474301b8a5
Jerry G <jerry99george@gmail.com>
2024-09-24 15:02:19 +0100

    added cow and raycaster

--- project_info.js
+++ project_info.js
@@ -2,118 +2,13 @@ projectInformation = [
     {
         name:"Maze Escape Raycaster",
         description:"Small maze game that works with raycasting.",
-        file:"maze.html",
+        filename:"maze.html",
         image:"maze_demo.gif"
     },
     {
         name:"Cow Rotator",
         description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"TEST1",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },{
-        name:"TEST2",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
+        filename:"cow-rotator.html",
         image:"maze_demo.gif"
     }
-    ,{
-        name:"TEST3",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Maze Escape Raycaster",
-        description:"Small maze game that works with raycasting.",
-        file:"maze.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Cow Rotator",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"TEST1",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },{
-        name:"TEST2",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    }
-    ,{
-        name:"TEST3",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Maze Escape Raycaster",
-        description:"Small maze game that works with raycasting.",
-        file:"maze.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Cow Rotator",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"TEST1",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },{
-        name:"TEST2",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    }
-    ,{
-        name:"TEST3",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Maze Escape Raycaster",
-        description:"Small maze game that works with raycasting.",
-        file:"maze.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"Cow Rotator",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },
-    {
-        name:"TEST1",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    },{
-        name:"TEST2",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    }
-    ,{
-        name:"TEST3",
-        description:"Rotate a cow. WASDQE to control. Best used on mobile.",
-        file:"cow-rotator.html",
-        image:"maze_demo.gif"
-    }
-
 ]
